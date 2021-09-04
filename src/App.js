a43e6076570f92146f88e08505e67b75ba17ed00
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-09-04 15:06:09 +0900

    use ReactHook!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-
+function addTask(name){
+  alert(name);
+}
 
 function App(props){
   console.log(props);
@@ -19,7 +21,7 @@ function App(props){
   return(
     <div className="todoapp stack-large">
     <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
     <div className="filters btn-group stack-exception">
      <FilterButton/>
      <FilterButton/>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 
 
-export default function Form(prosps){
+
+
+
+export default function Form(props){
+// useStateは、stateの初期値と、初期値を更新するための関数を返す！
+// 下のコードは、name=Use hooks ,setName= setState({name:xx})という二つの
+// 返り値を[]内に格納している。
+    const [name,setName]=useState("");
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log("value is :"+e.target.value);
+        props.addTask("Say Hello!");
+    }
+
     return(
-        <form>
-      <h2 className="label-wrapper">
-        <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
-        </label>
-      </h2>
+      <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+            </label>
+        </h2>
       <input
         type="text"
         id="new-todo-input"
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
