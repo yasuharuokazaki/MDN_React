7d5406088f420501242ef2f717554be5db26e834
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-09-04 09:53:43 +0900

    use props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Todo from "./components/Todo";
 
 function App(){
   return(
@@ -46,54 +47,10 @@ function App(){
       className="todo-list stack-large stack-exception"
       aria-labelledby="list-heading"
     >
-      <li className="todo stack-small">
-        <div className="c-cb">
-          <input id="todo-0" type="checkbox" defaultChecked={true} />
-          <label className="todo-label" htmlFor="todo-0">
-            Eat
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">Eat</span>
-          </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">Eat</span>
-          </button>
-        </div>
-      </li>
-      <li className="todo stack-small">
-        <div className="c-cb">
-          <input id="todo-1" type="checkbox" />
-          <label className="todo-label" htmlFor="todo-1">
-            Sleep
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">Sleep</span>
-          </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">Sleep</span>
-          </button>
-        </div>
-      </li>
-      <li className="todo stack-small">
-        <div className="c-cb">
-          <input id="todo-2" type="checkbox" />
-          <label className="todo-label" htmlFor="todo-2">
-            Repeat
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">Repeat</span>
-          </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">Repeat</span>
-          </button>
-        </div>
-      </li>
+      <Todo name="Eat" completed={true} id="todo-0" />
+      <Todo name="Sleep" completed={false} id="todo-1" />
+      <Todo name="Repeat" completed={false} id="todo-2" />
+      
     </ul>
   </div>
 );
